83ecfe9a70a961757404bc7f02c5dc6117aa591f
ChaddFrasier <chaddfrasier20@gmail.com>
2020-07-14 10:12:35 -0700

    added caption width options

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -3,11 +3,32 @@
 $(document).ready(()=>{
     // variables for basic js functions
     var svg = document.getElementById("figurecontainer");
-    var bgpicker = document.getElementById("backgroundcolor")
+    var caption = document.getElementById("captionbox");
+    var captionlocation = document.getElementById("captionlocation");
+    var bgpicker = document.getElementById("backgroundcolor");
     
+    // set background right away when page loads
     setSVGBackground(svg, bgpicker.value);
 
-    // moving inner menus up and down
+
+    /** CAPTION TOOLS LISTENERS */
+    $('#captionwidth').on("change", function(event){
+        caption.style.width = this.value;
+    });
+
+    $('#captionheight').on("change", function(event){
+        caption.style.height = this.value;
+    });
+
+    $('#captionxcoord').on("change", function(event){
+        captionlocation.setAttribute("x",this.value);
+    });
+
+    $('#captionycoord').on("change", function(event){
+        captionlocation.setAttribute("y",this.value);
+    });
+
+    /* Show and hide contents of the tool windows works generically so we can add more later */
     $('button.windowminimizebtn').click((event) => {
         if(event.target.parentElement.nextElementSibling.style.height == '0%'){
             event.target.parentElement.nextElementSibling.style.height = event.target.parentElement.nextElementSibling.style.maxHeight;
@@ -18,22 +39,28 @@ $(document).ready(()=>{
             event.target.innerHTML = '▼';
         }
     });
+    // close tools window if the title bar is clicked
+    $(".windowoptionsbar").on("click", (event) => {
+        let btn = event.target.lastElementChild;
+        if( btn ){
+            btn.click();
+        }
+    });
 
+    /** simple redirect for when the user clicks the usgs atrogeology in the footer */
     $('.rightfooter').click(() => {
         window.open("https://www.usgs.gov/centers/astrogeology-science-center", '_blank');
     });
 
+    // TODO: fix the transition issues happening with this now
     $('button.toolboxminimizebtn').click((event) => {
         let toolbox = document.getElementById('toolbox');
-        let editbox = document.getElementById('editbox');
         if( toolbox.classList.contains('closed') ){
             toolbox.classList.remove('closed');
-            editbox.style.width = "auto";
             event.target.innerHTML = "◄";
         }
         else{
             toolbox.classList.add('closed');
-            editbox.style.width = "95%";
             event.target.innerHTML = "►";
         }
     });
@@ -45,28 +72,33 @@ $(document).ready(()=>{
         svg.setAttribute("viewBox", "0 0 " + tmp[0] + ' ' + tmp[1]);
     });
 
-    // TODO: need to finish changing the svg boxes and stuff when these are changed
+    /**
+     * Changes the background color of the editing area.
+     * will be visible when exported
+     */
     $('#backgroundcolor').on("change", () => {
         setSVGBackground(svg, bgpicker.value)
     });
 
-// TODO:
-    $('#framesizeselect').on("change", (event) => {
-        console.log("UPDATE THE CAPTION AND IMAGE DIMENSIONS")
-    });
-// TODO:
+
+
+    /** Annotation buttons */
+
+    // TODO: add the north arrow stuff here
     $('#northarrowopt').on("mouseup", (event) => {
         let checkbox = event.target;
 
         console.log("ADD AND REMOVE NORTH ICON")
     });
-// TODO:
+    
+    // TODO: add sun azimuthal stuff here
     $('#sunarrowopt').on("mouseup", (event) => {
         let checkbox = event.target;
 
         console.log("ADD AND REMOVE SUN ICON")
     });
-// TODO:
+    
+    // TODO: add observer stuff here
     $('#observerarrowopt').on("mouseup", (event) => {
         let observer = event.target;
 
@@ -74,6 +106,8 @@ $(document).ready(()=>{
     });
 }); // end of jquery functions
 
+
+/* Helper functions */
 function setSVGBackground(svg, color){
     svg.style.background = color;
 }
