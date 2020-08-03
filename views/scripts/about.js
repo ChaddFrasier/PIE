a6727979104855d357aa05aa14e80b09f11526eb
ChaddFrasier <chaddfrasier20@gmail.com>
2020-08-03 12:10:37 -0700

    added about description

--- views/scripts/about.js
+++ views/scripts/about.js
@@ -2,13 +2,19 @@
 
 function writeContent(){
     const contentArr = [
-        String("Born from the Planetary Image Caption Writer Project by NAU Capstone and the USGS,\
-    this project aims to simplify the figure making process for many types of planetary researchers. Using ISIS Cube \
-    files or Tiff files we can help automate some of the more tedious parts of creating a scientificly \
-    acceptable figure."),
-        String("PIE allows you to create publication ready figures much faster than photoshop alone while \
-    providng a much simplier user interface than what is given by ArcGIS"),
-        String("Use the Image Button to add ISIS cubes, GeoTIFFs, or still images to the figure")];
+        String("Born from the <i><b>Planetary Image Caption Writer Project</b></i> by Laszlo Kestay and the USGS,\
+    this project aims to simplify the figure making process for planetary researchers that use \
+    <a class='descanchor', href='https://astrocloud.wr.usgs.gov/' , target='_blank'>POW</a> and <a \
+    class='descanchor', href='https://pilot.wr.usgs.gov/' , target='_blank'>PILOT</a> by the USGS. Using ISIS \
+     Cube files or GeoTIFF files, PIE helps automate some of the more tedious parts of creating a scientificly \
+    acceptable figure.<br/><br/> PIE helps automate image annotation by digging through the planeatary data files and \
+    setting the icons accordingly. PIE also allows users write captions and place them directly over the image or\
+     export all layers seperatly."),
+        String("PIE allows you to create publication ready figures much faster than similar products while \
+    providng a much simplier user interface given by ArcGIS and similar applications. PIE specifically focuses on \
+    GeoTIFF and ISIS Cube products because it it intentded for use by planetary scientists."),
+        String("Use the Image Button to add ISIS cubes, GeoTIFFs, or still images to the figure <br/> \
+        <pre>THIS IS A SPECIAL INSTRUCTION BOX</pre>")];
 
     let contentPrefix = "aboutcontent";
     contentArr.forEach(string => {
