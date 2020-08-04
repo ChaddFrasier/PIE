801bd03113960ba0b3918f05943f83678f39239f
ChaddFrasier <chaddfrasier20@gmail.com>
2020-08-04 16:47:18 -0700

    added all instructions for PILOT

--- views/scripts/about.js
+++ views/scripts/about.js
@@ -2,21 +2,26 @@
 
 function writeContent(){
     const contentArr = [
-        String("Born from the <i><b>Planetary Image Caption Writer Project</b></i> by Laszlo Kestay and the USGS,\
-    this project aims to simplify the figure making process for planetary researchers that use \
-    <a class='descanchor', href='https://astrocloud.wr.usgs.gov/' , target='_blank'>POW</a> and <a \
-    class='descanchor', href='https://pilot.wr.usgs.gov/' , target='_blank'>PILOT</a> by the USGS. Using ISIS \
-     Cube files or GeoTIFF files, PIE helps automate some of the more tedious parts of creating a scientificly \
-    acceptable figure.<br/><br/> PIE helps automate image annotation by digging through the planeatary data files and \
-    setting the icons accordingly. PIE also allows users write captions and place them directly over the image or\
-     export all layers seperatly."),
-        String("PIE allows you to create publication ready figures much faster than similar products while \
-    providng a much simplier user interface given by ArcGIS and similar applications. PIE specifically focuses on \
-    GeoTIFF and ISIS Cube products because it it intentded for use by planetary scientists."),
-        String("Use the Image Button to add ISIS cubes, GeoTIFFs, or still images to the figure <br/> \
-        <pre>1. Find a planetary product with <a class='descanchor', href='https://pilot.wr.usgs.gov/'>PILOT</a>.\
-        </pre>-<pre>2. Select the POW export function in PILOT.</pre>-<pre>3. Select the proper POW configuration.\
-        </pre>-<pre>4. Navigate to the link that is given to you when POW finished processing your files.</pre><br/>")];
+        String("<div style='text-align:left'>PIE allows you to create publication ready figures much faster than similar products while \
+        providng a much simplier user interface given by ArcGIS and similar applications. PIE specifically focuses on \
+        GeoTIFF and ISIS Cube products because it it intentded for use by planetary scientists.</div>"),
+        String(" \
+        <pre>1. <i><b>Navigate to <a class='descanchor', href='https://pilot.wr.usgs.gov/'>PILOT</a></b></i>.\
+        </pre><br/><pre>2. <i><b>Select</b></i> the <i><b>Planetary Body</b></i> you would like to search within.</pre><img alt='pilotimage1' class='pilotstepsimage' \
+        src='images/pilot1.png'/><br/>\
+        <pre>3. <i><b>Click</b></i> the <i><b>Map</b></i> tab to see the whole pds map.</pre><img alt='pilotimage3' class='pilotstepsimage' \
+        src='images/pilot3.png'/><br/> \
+        <pre>4. <i><b>Click</b></i> the <i><b>Polygon</b></i> button to the left of the map.</pre><img alt='pilotimage4' class='pilotstepsimage' \
+        src='images/pilot4.png'/><br/> \
+        <pre>5. <i><b>Draw</b></i> an area to search for a product image.</pre><img alt='pilotimage5' class='pilotstepsimage' \
+        src='images/pilot5.png'/><br/> \
+        <pre>6. <i><b>Check the boxes of the images</b></i> you like and <i><b>Click</b></i> the <i><b>Arrow Button</b></i> in the top right when you are ready.</pre><img alt='pilotimage6' class='pilotstepsimage' \
+        src='images/pilot6.png'/><br/> \
+        <pre>7. <i><b>Click</b></i> the bubble for <i><b>Project on the Web (POW)</b></i> and then <i><b>Click Go!</b></i>.</pre><img alt='pilotimage7' class='pilotstepsimage' \
+        src='images/pilot7.png'/><br/> \
+        <pre>8. <i><b>Click Submit</b></i>.</pre><img alt='pilotimage8' class='pilotstepsimage pilotstepsimage-sm' \
+        src='images/pilot8.png'/><br/> \
+        <pre>9 - HOW EVER LONG IT TAKES. Coming in further update.</pre><br/>")];
 
     let contentPrefix = "aboutcontent";
     contentArr.forEach(string => {
