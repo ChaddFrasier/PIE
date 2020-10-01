4623167a19e41c7224fd9ac5bac0aac052e7590a
Frasier <cfrasier@igswzawglt1178.local>
2020-09-30 17:16:10 -0700

    documented function

--- public/javascripts/DraggableArea.js
+++ public/javascripts/DraggableArea.js
@@ -115,17 +115,18 @@ function DraggableArea( objectbox=undefined )
 
         // ---------------- Private Listener Functions ( Only Needed on Successful Init ) --------------------------
         /**
-         * 
-         * @param {*} event 
+         * @function dragObject
+         * @param {_Event} event the mouse move event to get the location of the mouse
+         * @description Function that adjusts the element's transform that is currently being clicked on inside the DraggableArea.
          */
         function dragObject ( event )
         {
             // transform the mouse event location to the svg subspace
             let svgP = createSVGP(event.clientX, event.clientY)
 
-            if( draggingIcon.nodeName == "g" )
+            if( draggingIcon.nodeName == "g" ) // 'groups' or 'g' nodes house complex icons like the north arrow
             {
-                // get the scaled points and the mouse event
+                // get the scaled points from the svg transformed points and the icon dimensions
                 let scaledX = getScaledPoint(svgP.x, getTransform("scale", draggingIcon), draggingIcon.getBBox().width)
                 let scaledY = getScaledPoint(svgP.y, getTransform("scale", draggingIcon), draggingIcon.getBBox().height)
 
@@ -137,45 +138,50 @@ function DraggableArea( objectbox=undefined )
             }
             else if( draggingIcon.nodeName == "rect" )
             {
+                // get the current mouse location with no body to the rect
                 currentX = getScaledPoint(svgP.x, 1, 1)
                 currentY = getScaledPoint(svgP.y, 1, 1)
 
+                // get the x and y of the new point using the diffrence of the old and current mouse locations
                 let y = Number(draggingIcon.getAttribute("y")) + (currentY - oldY)
                 let x = Number(draggingIcon.getAttribute("x")) + (currentX - oldX)
 
+                // set the icon x and y
                 draggingIcon.setAttribute("x", x)
                 draggingIcon.setAttribute("y", y)
-
+                // update the input fields
                 updateInputField(draggingIcon.getAttribute("id"), x, y)
 
             }
             else if( draggingIcon.nodeName == "line" )
             {
-
-                // get first mouse position
+                // get transformed mouse position
                 currentX = getScaledPoint(svgP.x, 1, 1)
                 currentY = getScaledPoint(svgP.y, 1, 1)
 
+                // get the new location of all the points of the line element
                 let x1 = Number(draggingIcon.getAttribute("x1")) + (currentX - oldX)
                 let y1 = Number(draggingIcon.getAttribute("y1")) + (currentY - oldY)
                 let x2 = Number(draggingIcon.getAttribute("x2")) + (currentX - oldX)
                 let y2 = Number(draggingIcon.getAttribute("y2")) + (currentY - oldY)
                 
+                // set the new location of the line and uodate the line input fields
                 draggingIcon.setAttribute("x1", x1)
                 draggingIcon.setAttribute("y1", y1)
                 draggingIcon.setAttribute("x2", x2)
                 draggingIcon.setAttribute("y2", y2)
-
                 updateInputField(draggingIcon.getAttribute("id"), x1, y1, x2, y2)
             }
 
+            // update the old location to the current after every cycle
             oldX = currentX;
             oldY = currentY;
         }
         
         /**
-         * @function
+         * @function endDrag
          * @param {void}
+         * @description Trys to remove all the event listeners from the svgcontainer to change the location of the icon and then erases the memory at the variables
          */
         function endDrag( )
         {
@@ -201,20 +207,25 @@ function DraggableArea( objectbox=undefined )
         }
 
         /**
-         * 
-         * @param {_Event} event 
+         * @function dragHandler
+         * @param {_Event} event the mouse move event
+         * @description This function drags the elemnt inside the DraggableArea that was clicked on 
          */
         function dragHandler ( event )
         {
             let svgcontainer = DragBoxContainer
 
+            // IF THE NODE IS AN IMAGE IGNORE
+            // TODO: consider removeing this because it could be a better useage if the user can drag images as well as icons and lines
             if(event.target.nodeName != "image")
             {
+                // get the parent container of the target if it is valid
                 draggingIcon = getIconParentContainer( event.target )
 
                 console.log("The dragging object is: ")
                 console.log(draggingIcon)
 
+                // if the drag icon is found to be valid then initiate the dragging functions
                 if( draggingIcon != null )
                 {   
                     // requires svgHelper.js
@@ -228,19 +239,17 @@ function DraggableArea( objectbox=undefined )
 
                     draggingIcon.classList.add('dragging')
                     svgcontainer.classList.add('dragging')
-
                 }
             }
         }
         // ---------------- End Private functions 2 --------------------------
 
 
-        // add the main listener to the object target by Draggable
+        // add the main listener to the object targeted by DraggableArea() init function
         DragBoxContainer.addEventListener("mousedown", dragHandler )
 
-        // return the DraggableArea object
+        // return the DraggableArea object function helpers
         return {
-
             /**
              * @function getContainerObject
              * @description return the DOM object assosiated with the DraggableObject
@@ -259,7 +268,7 @@ function DraggableArea( objectbox=undefined )
     }
     else
     {
-        // on failure return the failed nodeName
+        // on failure return the failed nodeName that could not resolve to a proper DraggableArea
         return {
             NodeName: objectbox.nodeName
         };
