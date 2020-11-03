92e373d747ec77a455b52b53988ba958f12ca4a4
Frasier <cfrasier@igswzawglt1178.local>
2020-11-03 12:53:11 -0700

    added outline for image tool

--- cypress/integration/PIE/tool.spec.js
+++ cypress/integration/PIE/tool.spec.js
@@ -4,6 +4,7 @@ context('Tools Tests', () => {
     beforeEach(() => {
       cy.visit('http://localhost:8080/')
       cy.get("#addcaptionbtn").click()
+      cy.get("#addimagebtn").click()
     })
   
     // https://on.cypress.io/interacting-with-elements
@@ -24,4 +25,11 @@ context('Tools Tests', () => {
         cy.get("text[data-cy='caption']>tspan>tspan").should("have.html", "This is a test to see if typing inside of the editable textarea changes html")
       });
     });
+
+
+    describe("Image Tests -> ", () =>{ 
+        it( "Should add caption when add caption button is clicked." ,() => {
+          cy.get("image.holder").should("exist")
+        });
+      });
 });
