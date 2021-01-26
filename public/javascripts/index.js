c56695b4a657d784324d4794beeab324a5287ba3
Frasier <cfrasier@igswzawglt1178.local>
2021-01-26 13:07:33 -0700

    adding proper id regexp and css

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1142,6 +1142,7 @@ $( function()
             toolsarea = document.createElement("div"),
             powIdLabel = document.createElement("label"),
             powIdInput = document.createElement("input"),
+            powIdSubmitBtn = document.createElement("button"),
             imagesvg = document.createElementNS(NS.svg, "image");
 
         // create the main holder group for the image
@@ -1182,12 +1183,30 @@ $( function()
         toolsarea.setAttribute("objectid", imageId)
 
         powIdLabel.innerHTML = "Enter POW Job Id:";
+        powIdInput.name = "powIdInput"
         
-        powIdInput.placeholder = "125674923678";
+        powIdInput.placeholder = "4580b40493f62edca422fb1958d7635";
+
+        powIdSubmitBtn.addEventListener("click", (e) => {
+            let powId = powIdInput.value;
+            const powRegExp = /(\d|\w){31}$/
+            if( powRegExp.test(powId) )
+            {
+                console.log("THIS IS WHERE I NEED TO MAKE THE REQUEST TO THE SERVER TO FIND THE JOB ID FOLDER")
+            }
+            else
+            {
+                console.log("THIS CANNOT BE A POW ID.")
+                // TODO: add an indicator for the user that the id is wrong
+            }
+        });
+
+        powIdSubmitBtn.innerHTML = "Submit Id"
 
         toolsarea.append(
             powIdLabel,
-            powIdInput
+            powIdInput,
+            powIdSubmitBtn
         )
 
         // set caption id on all input elements
