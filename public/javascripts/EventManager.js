bcf19d0f25c5dc679f019ebb78e03104edffe09a
Frasier <cfrasier@igswzawglt1178.local>
2021-05-18 14:07:04 -0700

    broke event manager into another class

---
+++ public/javascripts/EventManager.js
@@ -0,0 +1,38 @@
+"use strict"
+
+class EventManager {
+    RunningEvent = undefined;
+
+    activateEvent() {
+        // if there is no event running then return true otherwise false
+        if( this.RunningEvent )
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    setEventFlag( val ) {
+        this.RunningEvent = val;
+    }
+
+    deactivateBtn( id ) {
+        var btn = document.getElementById(id)
+        if( btn )
+        {
+            btn.classList.add("disabled")
+        }
+    }
+
+    reactivateBtn( id ) {
+        var btn = document.getElementById(id)
+        if( btn )
+        {
+            btn.classList.remove("disabled")
+        }
+    }
+    
+}
